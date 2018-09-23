8bd5a1071e7c68eaaf584040f088cdb59555367b
Bryant-Anjos <40769960+Bryant-Anjos@users.noreply.github.com>
2018-09-23 01:10:29 -0300

    Adição de seleção de cor

--- cursophp/idade.php
+++ cursophp/idade.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
   <head>
+    <?php
+      $nome = isset($_GET["nome"])?$_GET["nome"]:"Ninguém";
+      $ano = isset($_GET["ano"])?$_GET["ano"]:0;
+      $idade = isset($_GET["idade"])?$_GET["idade"]:0;
+      $tempo = isset($_GET["tempo"])?$_GET["tempo"]:0;
+      $color = isset($_GET["color"])?$_GET["color"]:"black";
+    ?>
     <meta charset="utf-8">
-    <title></title>
+    <title><?php echo "Idade de ", $nome; ?></title>
+    <style>
+      span {
+        color: <?php echo $color; ?>;
+      }
+    </style>
   </head>
   <body>
 
+    <a href="index.html">Calcular outra vez</a>
+
     <?php
-    $nome = $_GET["nome"];
-    $ano = $_GET["ano"];
-    $idade = $_GET["idade"];
-    $tempo = $_GET["tempo"];
 
     for ($i=0; $i < $tempo; $i++) {
       if ($idade < 16) {
-        echo "<p>Em <strong>$ano <span style='color: red;'>$nome</span></strong> tem <strong>$idade</strong> anos. <strong><span style='color: red;'>$nome</span></strong> não pode dirigir e nem votar<p>";
+        echo "<p>Em <strong>$ano <span>$nome</span></strong> tem <strong>$idade</strong> anos. <strong><span>$nome</span></strong> não pode dirigir e nem votar<p>";
       } elseif ($idade <18) {
-        echo "<p>Em <strong>$ano <span style='color: red;'>$nome</span></strong> tem <strong>$idade</strong> anos. <strong><span style='color: red;'>$nome</span></strong> não pode dirigir mas pode votar<p>";
+        echo "<p>Em <strong>$ano <span>$nome</span></strong> tem <strong>$idade</strong> anos. <strong><span>$nome</span></strong> não pode dirigir mas pode votar<p>";
       } else {
-        echo "<p>Em <strong>$ano <span style='color: red;'>$nome</span></strong> tem <strong>$idade</strong> anos. <strong><span style='color: red;'>$nome</span></strong> pode dirigir e votar<p>";
+        echo "<p>Em <strong>$ano <span>$nome</span></strong> tem <strong>$idade</strong> anos. <strong><span>$nome</span></strong> pode dirigir e votar<p>";
       }
 
       $ano++;
