a36b952f1f9a3cad0daac683795703d0f01813ef
Bryant-Anjos <bryant_mx@hotmail.com>
2018-10-27 23:51:06 -0300

    Melhorias no design da página de cadastro

--- interdisciplinar/cadastro.php
+++ interdisciplinar/cadastro.php
@@ -8,6 +8,8 @@
 
   <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
 
+  <link href="css/sb-admin.css" rel="stylesheet">
+
   <script>
     // Dropdown keep open: para impedir o dropdow de fechar ao clicar dentro dele
       $('.dropdown.keep-open').on({
@@ -17,19 +19,19 @@
       });
 
       function enviarCadastro() {
-        if (document.getElementById('confirmPassCadastro').value == document.getElementById('passCadastro').value) {
-          document.getElementById('buttonCadastrar').className = "btn btn-primary";
+        if (document.getElementById('confirmPassword').value == document.getElementById('inputPassword').value) {
+          document.getElementById('buttonCadastrar').className = "btn btn-primary btn-block";
           document.getElementById('buttonCadastrar').disabled = false;
         } else {
-          document.getElementById('buttonCadastrar').className = "btn btn-primary disabled";
+          document.getElementById('buttonCadastrar').className = "btn btn-primary btn-block disabled";
           document.getElementById('buttonCadastrar').disabled = true;
         }
       }
       function isValid() {
-        if (document.getElementById('confirmPassCadastro').value == document.getElementById('passCadastro').value) {
-          document.getElementById('confirmPass').className = "form-group was-validated"
+        if (document.getElementById('confirmPassword').value == document.getElementById('inputPassword').value) {
+          document.getElementById('confirmPass').className = "form-label-group was-validated"
         } else {
-          document.getElementById('confirmPass').className = "form-group"
+          document.getElementById('confirmPass').className = "form-label-group"
         }
         enviarCadastro();
       }
@@ -38,102 +40,75 @@
 
 </head>
 
-<body>
+<body class="bg-dark">
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <div class="dropdown keep-open">
-        <form class="form-inline my-2 my-lg-0 was-validated">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-              <a class="nav-link" href="#" id="loginDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <button type="button" class="btn btn-success my-2 my-sm-0">Login</button>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="loginDropdown">
-
-                <div class="form-group col-md-4 mb-3">
-                  <label for="userLogin">E-mail</label>
-                  <input type="email" class="form-control is-invalid" id="userLogin" placeholder="email@example.com" required name="valor1">
+  <div class="container">
+    <div class="card card-register mx-auto mt-5">
+      <div class="card-header">Registrar uma conta</div>
+      <div class="card-body">
+        <form  action="bd/cadastrando.php" method="post" name="signup">
+          <div class="form-group was-validated">
+            <div class="form-row">
+              <div class="col-md-12">
+                <div class="form-label-group">
+                  <input type="text" id="firstName" name="nameCadastro" class="form-control" placeholder="Nome da Empresa" required autofocus="autofocus">
+                  <label for="firstName">Nome da Empresa</label>
                 </div>
-
-                <div class="form-group col-md-4 mb-3">
-                  <label for="passwordLogin">Senha</label>
-                  <input type="password" class="form-control is-invalid" id="passwordLogin" placeholder="Password" required name="valor1">
-                </div>
-
-                <div class="col-md-4 mb-2">
-                  <button class="btn btn-primary" type="submit">Entrar</button>
-                </div>
-
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="cadastro.php">Novo por aqui? Cadastre-se</a>
-                <a class="dropdown-item" href="#">Esqueceu sua senha?</a>
               </div>
-            </li>
-          </ul>
+
+            </div>
+          </div>
+          <div class="form-group was-validated">
+            <div class="form-label-group">
+              <input type="email" id="inputEmail" name="mailCadastro" class="form-control" placeholder="Endereço de e-mail" required>
+              <label for="inputEmail">Endereço de e-mail</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col-md-6">
+                <div class="form-label-group was-validated">
+                  <input type="password" id="inputPassword" name="passCadastro" class="form-control" placeholder="Password" required oninput="isValid()">
+                  <label for="inputPassword">Senha</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-label-group" id="confirmPass">
+                  <input type="password" id="confirmPassword" name="confirmPassCadastro" class="form-control is-invalid" placeholder="Confirm password" required oninput="isValid()">
+                  <label for="confirmPassword">Confirmar senha</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col-md-12">
+                <label>Tipo da empresa</label>
+                  <div class="btn-group btn-block btn-group-toggle" data-toggle="buttons">
+                    <label class="btn btn-outline-success active" for="radioFornecedor" style="width: 50%;">Produtor
+                    <input type="radio" name="radioEmpresa" id="radioFornecedor" autocomplete="off" value="1" checked>
+                    </label>
+                    <label class="btn btn-outline-success" for="radioEcommerce" style="width: 50%;">E-commerce
+                    <input type="radio" name="radioEmpresa" id="radioEcommerce" autocomplete="off" value="2">
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button id="buttonCadastrar" class="btn btn-primary btn-block" type="submit" disabled>Registrar</button>
+
         </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="login.html">Página de login</a>
+          <a class="d-block small" href="forgot-password.html">Esqueceu sua senha?</a>
         </div>
       </div>
-  </nav>
+    </div>
+  </div>
 
-  <form action="bd/cadastrando.php" method="post" name="signup">
-    <div class="form-group was-validated">
-      <label for="nameCadastro">Nome da Empresa</label>
-      <input type="text" class="form-control col-md-2 mb-3" id="nameCadastro" name="nameCadastro" placeholder="Nome" min="4" maxlength="30" required>
-    </div>
-    <div class="form-group was-validated">
-      <label for="mailCadastro">Email</label>
-      <input type="email" class="form-control col-md-2 mb-3" id="mailCadastro" name="mailCadastro" placeholder="mail@example.com" min="4" maxlength="30" required>
-    </div>
-    <div class="form-group was-validated">
-      <label for="passCadastro">Senha</label>
-      <input type="password" class="form-control col-md-2 mb-3" id="passCadastro" name="passCadastro" placeholder="Senha" min="4" maxlength="30" required oninput="isValid()">
-    </div>
-    <div class="form-group" id="confirmPass">
-      <label for="confirmPassCadastro">Confirme sua senha</label>
-      <input type="password" class="form-control is-invalid col-md-2 mb-3" id="confirmPassCadastro" placeholder="Senha" min="4" maxlength="30" required oninput="isValid()">
-    </div>
-      <p>Tipo da empresa</p>
-      <div class="btn-group btn-group-toggle btn-block mb-3" data-toggle="buttons">
-        <label class="btn btn-outline-dark active" for="radioFornecedor">Produtor
-        <input type="radio" name="radioEmpresa" id="radioFornecedor" autocomplete="off" value="1" checked>
-        </label>
-        <label class="btn btn-outline-dark" for="radioEcommerce">E-commerce
-        <input type="radio" name="radioEmpresa" id="radioEcommerce" autocomplete="off" value="2">
-        </label>
       </div>
-    <div class="form-group was-validated">
-    <button id="buttonCadastrar" class="btn btn-primary disabled" type="submit">Enviar</button>
     </div>
-  </form>
-
+  </div>
 
   <script src="node_modules/jquery/dist/jquery.min.js"></script>
   <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
